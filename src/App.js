6002814e0b4132f97742856e0455a7061c4cb2db
Delyc <d.twizeyima@alustudent.com>
2023-03-16 11:43:03 +0200

    Feat: Fixing deployment issues

--- src/App.js
+++ src/App.js
@@ -5,37 +5,22 @@ import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux';
 import { addTodo } from './redux/reducers/todoSlice';
 import { updateTodo } from './redux/reducers/todoSlice';
-import { markAsCompleted } from './redux/reducers/todoSlice';
 import { deleteTodo } from './redux/reducers/todoSlice';
 
-const getLocalStorage = () => {
-  let list = localStorage.getItem("list")
-  if (list) {
-    return (list = JSON.parse(localStorage.getItem("list")))
-  } else {
-    return []
-  }
-}
+
 function App() {
   const dispatch = useDispatch()
   const [name, setName] = useState("")
-  const [list, setList] = useState(getLocalStorage())
   const [isEditing, setIsEditing] = useState(false)
   const [editID, setEditiID] = useState(null)
-  const todos = useSelector((state) => state.todos )
-  console.log("todos redux", todos)
-  useEffect(() => {
-    localStorage.setItem("list", JSON.stringify(list))
-  }, [list])
-
-
+  const todos = useSelector((state) => state.todos)
+ 
   const handleSubmit = (e) => {
     e.preventDefault()
     if (!name) {
       alert("please enter a task")
     } else if (name && isEditing) {
-    dispatch(updateTodo({id: editID, title: name }))
-      
+      dispatch(updateTodo({ id: editID, title: name }))
       setName("")
       setEditiID(null)
       setIsEditing(false)
@@ -49,35 +34,28 @@ function App() {
   }
   const removeItem = (id) => {
     dispatch(deleteTodo(id))
-    // setList(list.filter((item) => item.id !== id))
   }
   const editItem = (item) => {
-    console.log({item})
     setIsEditing(true)
     setEditiID(item.id)
     setName(item.title)
-    console.log("testing upate", item)
-
-    // const editItem = list.find((item) => item.id === id)
-    
-
   }
 
-  
+
 
   return (
     <>
-    <section className='flex flex-col items-center gap-10 py-20'>
-      <h1 className='text-4xl uppercase font-bold'>react todo</h1>
-      <form onSubmit={handleSubmit} className="flex gap-4">
-        <input type="text" placeholder='add task' onChange={(e) => setName(e.target.value)} value={name} className="underline-none border py-4 px-10 rounded shadow-lg" />
-        <button className='bg-blue-600 py-3 px-5 text-white font-medium rounded' type='submit'>{isEditing ? "edit" : "add"}</button>
-      </form>
-      {todos.length > 0 && (
-        <div className='flex flex-col gap-10'>
-          <List  items={todos} removeItem={removeItem} editItem={editItem} />
-        </div>
-      )}
+      <section className='flex flex-col items-center gap-10 py-20'>
+        <h1 className='text-4xl uppercase font-bold'>react todo</h1>
+        <form onSubmit={handleSubmit} className="flex gap-4">
+          <input type="text" placeholder='add task' onChange={(e) => setName(e.target.value)} value={name} className="underline-none border py-4 px-10 rounded shadow-lg" />
+          <button className='bg-blue-600 py-3 px-5 text-white font-medium rounded' type='submit'>{isEditing ? "edit" : "add"}</button>
+        </form>
+        {todos.length > 0 && (
+          <div className='flex flex-col gap-10'>
+            <List items={todos} removeItem={removeItem} editItem={editItem} />
+          </div>
+        )}
       </section>
     </>
   );
